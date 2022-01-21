a4ffe936e4458ab8b649f3f38c23d77637f5f334
Jack Jones <jacktjones4@gmail.com>
2022-01-21 09:59:00 -0800

    WIP working on getting test 2 to pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 // Buisness logic
+// function beepBoop(number) {
+//   const newNumber = parseInt(number);
+//   if (newNumber === 0);
+//   return 0;
+// }
 function beepBoop(number) {
-  const newNumber = parseInt(number);
-  if (newNumber === 0);
-  return 0;
+  const newNumbers = parseInt(number);
+  const array = newNumbers;
+
+  newNumbers.range(number).split("");
+  return newNumbers[i] + 1;
 }
 
+//   for (let index = 0; index < newNumbers.length; index += 1) {
+//     newNumbers[i] = newNumbers[i] + 1;
+//   }
+//   newNumbers.forEach(function(number) {
+//     return newNumbers
+//   }
+// }
 
 // UI
